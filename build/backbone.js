584506f447d731a2a2306008b97d1437627bebe2
RickyTaro <72301134+reverse3@users.noreply.github.com>
2024-11-03 02:29:36 +0800

    Update backbone.js

--- build/backbone.js
+++ build/backbone.js
@@ -1,1 +1,9 @@
-1
+// Open music list when playlist icon is clicked
+document.querySelector('#open-playlist').addEventListener('click', function() {
+    document.querySelector('.music-list').classList.toggle('active');
+});
+
+// Close music list when close icon is clicked
+document.querySelector('#close').addEventListener('click', function() {
+    document.querySelector('.music-list').classList.remove('active');
+});
